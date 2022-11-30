85862e8462127cd683eb22a5d86ae3458f2c5940
Karl Hedayati <k.hedayati.10@gmail.com>
2022-11-30 15:40:53 -0500

    added new booking functionnality

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :bookings do
-  end
+  # resources :bookings do
+  # end
   # get '/bookings/:id/edit', to: 'bookings#edit', as: 'edit_booking'
-  patch '/bookings/:id', to: 'bookings#update'
+  # patch '/bookings/:id', to: 'bookings#update'
 
-  resources :articles
+  resources :articles do
+    resources :bookings, only: %i[new create]
+  end
+  resources :bookings, only: %i[index edit show update destroy]
 end

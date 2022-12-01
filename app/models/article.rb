a4e35a7e3908137daaf92e9a783f72586db287e1
Anne Fleur Bayiha <anne.bayiha@yahoo.com>
2022-12-01 00:02:17 -0500

    fixed banner btn, added preview articles home page and index cards

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,10 @@
 class Article < ApplicationRecord
+  CATEGORIES = %w[clothing accessories shoes].freeze
   has_many_attached :photos
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, length: { minimum: 20 }
+  validates :category, inclusion: { in: CATEGORIES }
 end
